ffc43d6bd350ff7014b2963215ac8cf78bd8977c
jeffhorus <jeffhorus19@gmail.com>
2015-11-11 21:20:15 +0700

    first configuration

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jelink on 11/11/15.
+ */
+import com.datastax.driver.*;
+
+public class MainClass {
+    public static void main (String[] args) {
+        System.out.println("finally");
+    }
+}
